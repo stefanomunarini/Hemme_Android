5df53a673792acd38709a963126c6f6fd6a4114d
Stefano Munarini <ssteee@gmail.com>
2014-03-27 12:56:10 +0100

    Activity Home, Login & Registration

--- Hemme/src/main/java/com/povodev/hemme/android/activity/RegistrationActivity.java
+++ Hemme/src/main/java/com/povodev/hemme/android/activity/RegistrationActivity.java
@@ -1,20 +1,75 @@
 package com.povodev.hemme.android.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.povodev.hemme.android.R;
+import com.povodev.hemme.android.adapter.RegistrationRoleAdapter;
 
 import roboguice.activity.RoboFragmentActivity;
+import roboguice.inject.InjectView;
 
 /**
  * Created by Stefano on 27/03/14.
  */
-public class RegistrationActivity extends RoboFragmentActivity{
+public class RegistrationActivity extends RoboFragmentActivity implements View.OnClickListener {
+
+    @InjectView(R.id.name_edittext)         private EditText mNameEditText;
+    @InjectView(R.id.surname_edittext)      private EditText mSurnameEditText;
+    @InjectView(R.id.email_edittext)        private EditText mEmailEditText;
+    @InjectView(R.id.password_edittext)     private EditText mPasswordEditText;
+    @InjectView(R.id.registration_button)   private Button mRegistrationButton;
+    @InjectView(R.id.role_spinner)          private Spinner mRoleSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_registration);
 
+        initComponents();
+        populateSpinner();
+    }
+
+    private void populateSpinner() {
+        mRoleSpinner.setAdapter(new RegistrationRoleAdapter());
+    }
+
+    private void initComponents() {
+        mRegistrationButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.registration_button:
+                getValues();
+                break;
+        }
+    }
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String imei;
+    private int role;
+    private void getValues() {
+        name = mNameEditText.getText().toString();
+        surname = mSurnameEditText.getText().toString();
+        email = mEmailEditText.getText().toString();
+        password = mPasswordEditText.getText().toString();
+        role = mRoleSpinner.getSelectedItemPosition();
+        imei = getImei();
+    }
+
+    private String getImei() {
+        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        return telephonyManager.getDeviceId();
     }
 }
---
+++ Hemme/src/main/java/com/povodev/hemme/android/adapter/RegistrationRoleAdapter.java
@@ -0,0 +1,76 @@
+package com.povodev.hemme.android.adapter;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.SpinnerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Stefano on 27/03/14.
+ */
+public class RegistrationRoleAdapter implements SpinnerAdapter {
+
+    private ArrayList<String> mSpinnerArray = new ArrayList();
+
+    public RegistrationRoleAdapter(){
+        mSpinnerArray.add("Tutor");
+        mSpinnerArray.add("Doctor");
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mSpinnerArray.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
